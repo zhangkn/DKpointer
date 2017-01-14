2b692969bfaa382f0d413746c4ec6a0bca956bbc
kevin <929118967@qq.com>
2017-01-14 16:10:27 +0800

    Initial Commit

---
+++ DKpointer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DKpointer
+//
+//  Created by devzkn on 14/01/2017.
+//  Copyright © 2017 hisun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
